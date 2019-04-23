87a96ba04a8b7e36a33b45a2fc2714c6d1c0c172
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-04-23 15:48:06 +0900

    merge漏れ

---
+++ rplugin/python3/key-seeker/main.py
@@ -0,0 +1,26 @@
+import neovim
+from .lib import seeker
+
+
+@neovim.plugin
+class KeySeeker(object):
+
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    @neovim.command("KeySeekerClip", sync=True)
+    def key_seeker_clip(self):
+        cursor_r = self.get_cursor_pos()[0]
+        lines = self.nvim.current.buffer[:cursor_r]
+        key = seeker.seek_key(lines, cursor_r - 1)
+        self.clip(key)
+        self.nvim.out_write('Clipped: ' + key + '\n')
+
+    def get_cursor_pos(self):
+        cursor_r, cursor_c = self.nvim.eval('getpos(".")[1:2]')
+        return cursor_r, cursor_c
+
+    def clip(self, text):
+        self.nvim.command('let @0="{}"'.format(text))
+        self.nvim.command('let @"="{}"'.format(text))
+        self.nvim.command('let @*="{}"'.format(text))
